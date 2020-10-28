45242bc6b29eeb0281b84f09a2ebf0ee0696007f
naina <nanu02d8@gmail.com>
2020-10-28 21:09:04 +0530

    Added current day to the browser via local server

--- app.js
+++ app.js
@@ -7,34 +7,13 @@ app.set('view engine', 'ejs');
 
 app.get("/", (req,res) => {
     const today = new Date();
-    let day = "";
     const currentDay = today.getDay();
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        default:
-            break;
+    let options = {
+        weekday : "long",
+        month : "long",
+        day : "numeric"
     }
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render('list', {kindOfday:day});
 })
