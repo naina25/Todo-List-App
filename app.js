87f44b96a89c2ef0d66c0c9afb4933668ed4ded2
naina <nanu02d8@gmail.com>
2020-10-28 18:54:36 +0530

    Started using Embedded Javascript

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req,res) => {
-    
+    const today = new Date();
+    let day = "";
+    if(today.getDay() === 0 || today.getDay() === 6){
+        day = "Weekend";
+    }
+    else{
+        day = "Weekday";
+    }
+    res.render('list', {kindOfday:day});
 })
 
-app.listen("3000", () => {
-    console.log("SErver has staretd on port 3000");
+app.listen(3000, () => {
+    console.log("Server has staretd on port 3000");
 })
