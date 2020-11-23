c0cb7f11d42fbca581a7fc1a1995d5e6169d5f42
naina <nanu02d8@gmail.com>
2020-11-23 17:23:41 +0530

    add todo list items to the database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { static } = require('express');
 const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose");
 
 const app = express();
 const listItems = ["Buy Food", "Cook Food", "Eat Food"];
@@ -11,10 +12,45 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-app.get("/", (req,res) => {
-    let day = date.getDate();
-    res.render('list', {listTitle:day , newListItems: listItems});
+mongoose.connect('mongodb://localhost/todolistDb', {useNewUrlParser: true, useUnifiedTopology: true });
+const itemSchema = new mongoose.Schema({
+  name: String
 })
+const Item = mongoose.model("Item", itemSchema);
+
+const goToStore = new Item({
+  name: "Go to store"
+})
+const buyFood = new Item({
+  name: "Buy Food"
+})
+const cookFood = new Item({
+  name: "Cook Food"
+})
+
+const defaultItems = [goToStore, buyFood, cookFood];
+
+Item.insertMany(defaultItems, err => {
+  if(err){
+    console.log(err);
+  }else{
+    console.log("items are successfully added to the database");
+  }
+})
+
+app.get("/", function(req, res) {
+
+  Item.find((err, results)=> {
+    if(err){
+      console.log(err);
+    }
+    else{
+        const day = date.getDate();
+        res.render("list", {listTitle: "day", newListItems: results});
+        console.log(results);
+    }
+  })
+});
 
 app.post('/', (req,res)=>{
     console.log(req.body);
@@ -39,6 +75,6 @@ app.post("/work",(req,res)=>{
     workList.push(item);
     res.redirect("/work");
 })
-app.listen(3000, () => {
-    console.log("Server has staretd on port 3000");
+app.listen(4000, () => {
+    console.log("Server has staretd on port 4000");
 })
